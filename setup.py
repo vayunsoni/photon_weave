df82ece8ddceb565f663c9e5af7aa9206255f159
Simon Sekavcnik <simon.sekavcnik@gmail.com>
2024-04-15 20:04:02 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="photon_weave",
-    version="0.0.1",
+    version="0.0.2",
     author="Simon Sekavčnik",
     author_email="simon.sekavcnik@tum.de",
     description="Fock Envelope Simulator",
